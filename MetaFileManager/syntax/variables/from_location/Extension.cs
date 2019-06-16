a68f2a4b863fbefffe2dcb108d3548a0b73356d3
Wojciech-Filowiat <woj-fil@o2.pl>
2019-06-16 19:21:42 +0200

    Add first file variables

---
+++ MetaFileManager/syntax/variables/from_location/Extension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DivineScript.syntax.variables.abstracts;
+using DivineScript.syntax.runtime;
+using System.IO;
+
+namespace DivineScript.syntax.variables.from_location
+{
+    class Extension : NamedVariable, IStringable
+    {
+        public Extension()
+        {
+            name = "extension";
+        }
+
+        public override string ToString()
+        {
+            string thiss = RuntimeVariables.GetInstance().GetValueString("this");
+            if (this.Equals(""))
+            {
+                return "";
+            }
+            string location = RuntimeVariables.GetInstance().GetValueString("location") + "//" + thiss;
+
+            try
+            {
+                string value = Path.GetExtension(@location);
+                return value;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
