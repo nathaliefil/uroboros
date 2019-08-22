6e0d38b925d8d37533e472b95bd43257d196aca1
Wojciech-Filowiat <woj-fil@o2.pl>
2019-08-22 21:12:41 +0200

    Refactor token merging

--- MetaFileManager/syntax/lexer/TokenModifier.cs
+++ MetaFileManager/syntax/lexer/TokenModifier.cs
@@ -7,6 +7,10 @@ namespace Uroboros.syntax.lexer
 {
     class TokenModifier
     {
+        private static TokenType[][] TOKENS_TO_MERGE;
+        // for declaration -> go down
+
+        
         public static List<Token> VariablesToNumeric (List<Token> tokens)
         {
             foreach (Token t in tokens)
@@ -40,7 +44,6 @@ namespace Uroboros.syntax.lexer
                                     t.SetToNumericConstant(SizeSufix.K);
                             }
                         }
-
                     }
                 }
             }
@@ -59,9 +62,6 @@ namespace Uroboros.syntax.lexer
 
         public static List<Token> MergeTokens (List<Token> tokens)
         {
-            // this method needs serious refactoring
-            /// todo
-
             List<Token> newTokens = new List<Token>() ;
             bool tokensMerged = false;
             int i = 0;
@@ -71,127 +71,17 @@ namespace Uroboros.syntax.lexer
                 for (i = 0; i < tokens.Count - 1; i++)
                 {
                     tokensMerged = false;
-                    if (tokens[i].GetTokenType() == tokens[i+1].GetTokenType())
+                    int count = TOKENS_TO_MERGE.Count();
+
+                    for (int j = 0; j < count; j++)
                     {
-                        if (tokens[i].GetTokenType() == TokenType.Plus)
+                        if (tokens[i].GetTokenType() == TOKENS_TO_MERGE[j][0] && tokens[i+1].GetTokenType() == TOKENS_TO_MERGE[j][1])
                         {
-                            newTokens.Add(new Token(TokenType.PlusPlus));
+                            newTokens.Add(new Token(TOKENS_TO_MERGE[j][2]));
                             tokensMerged = true;
-                        }
-                        if (tokens[i].GetTokenType() == TokenType.Minus)
-                        {
-                            newTokens.Add(new Token(TokenType.MinusMinus));
-                            tokensMerged = true;
-                        }
-                        if (tokens[i].GetTokenType() == TokenType.Equals)
-                        {
-                            newTokens.Add(new Token(TokenType.Equals));
-                            tokensMerged = true;
-                        }
-                        if (tokens[i].GetTokenType() == TokenType.And)
-                        {
-                            newTokens.Add(new Token(TokenType.And));
-                            tokensMerged = true;
-                        }
-                        if (tokens[i].GetTokenType() == TokenType.Or)
-                        {
-                            newTokens.Add(new Token(TokenType.Or));
-                            tokensMerged = true;
+                            break;
                         }
                     }
-                    if (tokens[i].GetTokenType() == TokenType.Bigger && tokens[i+1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.BiggerOrEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Smaller && tokens[i+1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.SmallerOrEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Equals && tokens[i+1].GetTokenType() == TokenType.Bigger)
-                    {
-                        newTokens.Add(new Token(TokenType.BigArrow));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Minus && tokens[i + 1].GetTokenType() == TokenType.Bigger)
-                    {
-                        newTokens.Add(new Token(TokenType.SmallArrow));
-                        tokensMerged = true;
-                    }
-                    if ((tokens[i].GetTokenType() == TokenType.Exclamation && tokens[i + 1].GetTokenType() == TokenType.Equals)||
-                        (tokens[i].GetTokenType() == TokenType.Is && tokens[i + 1].GetTokenType() == TokenType.Exclamation))
-                    {
-                        newTokens.Add(new Token(TokenType.NotEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Force && tokens[i + 1].GetTokenType() == TokenType.To)
-                    {
-                        newTokens.Add(new Token(TokenType.ForceTo));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Create && tokens[i + 1].GetTokenType() == TokenType.File)
-                    {
-                        newTokens.Add(new Token(TokenType.CreateFile));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Create && tokens[i + 1].GetTokenType() == TokenType.Directory)
-                    {
-                        newTokens.Add(new Token(TokenType.CreateDirectory));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Order && tokens[i + 1].GetTokenType() == TokenType.By)
-                    {
-                        newTokens.Add(new Token(TokenType.OrderBy));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Sleep && tokens[i + 1].GetTokenType() == TokenType.For)
-                    {
-                        newTokens.Add(new Token(TokenType.Sleep));
-                        tokensMerged = true;
-                    }
-
-
-                    if (tokens[i].GetTokenType() == TokenType.Plus && tokens[i + 1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.PlusEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Minus && tokens[i + 1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.MinusEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Multiply && tokens[i + 1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.MultiplyEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Divide && tokens[i + 1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.DivideEquals));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Percent && tokens[i + 1].GetTokenType() == TokenType.Equals)
-                    {
-                        newTokens.Add(new Token(TokenType.PercentEquals));
-                        tokensMerged = true;
-                    }
-
-
-
-                    if (tokens[i].GetTokenType() == TokenType.Is && tokens[i + 1].GetTokenType() == TokenType.Before)
-                    {
-                        newTokens.Add(new Token(TokenType.IsBefore));
-                        tokensMerged = true;
-                    }
-                    if (tokens[i].GetTokenType() == TokenType.Is && tokens[i + 1].GetTokenType() == TokenType.After)
-                    {
-                        newTokens.Add(new Token(TokenType.IsAfter));
-                        tokensMerged = true;
-                    }
-
-
 
                     if (tokensMerged)
                         i++;
@@ -200,15 +90,10 @@ namespace Uroboros.syntax.lexer
                 }
             }
             if (i < tokens.Count)
-            {
                 newTokens.Add(tokens[tokens.Count-1]);
-            }
 
             foreach (Token tok in newTokens)
-            {
                 tok.ChangeTokenType();
-            }
-
 
             return newTokens;
         }
@@ -245,6 +130,37 @@ namespace Uroboros.syntax.lexer
                     return SizeSufix.KK;
             }
             return SizeSufix.None;
+        }
+
+
+        static TokenModifier()
+        {
+            // first and second token is merged to third token
+            TOKENS_TO_MERGE = new TokenType[24][]; //   TOKEN     +     TOKEN    =>      MERGED TWO
+            TOKENS_TO_MERGE[0] = new TokenType[] { TokenType.Plus, TokenType.Plus, TokenType.PlusPlus };
+            TOKENS_TO_MERGE[1] = new TokenType[] { TokenType.Minus, TokenType.Minus, TokenType.MinusMinus };
+            TOKENS_TO_MERGE[2] = new TokenType[] { TokenType.Equals, TokenType.Equals, TokenType.Equals };
+            TOKENS_TO_MERGE[3] = new TokenType[] { TokenType.And, TokenType.And, TokenType.And };
+            TOKENS_TO_MERGE[4] = new TokenType[] { TokenType.Or, TokenType.Or, TokenType.Or };
+            TOKENS_TO_MERGE[5] = new TokenType[] { TokenType.Bigger, TokenType.Equals, TokenType.BiggerOrEquals };
+            TOKENS_TO_MERGE[6] = new TokenType[] { TokenType.Bigger, TokenType.Equals, TokenType.BiggerOrEquals };
+            TOKENS_TO_MERGE[7] = new TokenType[] { TokenType.Smaller, TokenType.Equals, TokenType.SmallerOrEquals };
+            TOKENS_TO_MERGE[8] = new TokenType[] { TokenType.Equals, TokenType.Bigger, TokenType.BigArrow };
+            TOKENS_TO_MERGE[9] = new TokenType[] { TokenType.Minus, TokenType.Bigger, TokenType.SmallArrow };
+            TOKENS_TO_MERGE[10] = new TokenType[] { TokenType.Exclamation, TokenType.Equals, TokenType.NotEquals };
+            TOKENS_TO_MERGE[11] = new TokenType[] { TokenType.Is, TokenType.Exclamation, TokenType.NotEquals };
+            TOKENS_TO_MERGE[12] = new TokenType[] { TokenType.Force, TokenType.To, TokenType.ForceTo };
+            TOKENS_TO_MERGE[13] = new TokenType[] { TokenType.Create, TokenType.File, TokenType.CreateFile };
+            TOKENS_TO_MERGE[14] = new TokenType[] { TokenType.Create, TokenType.Directory, TokenType.CreateDirectory };
+            TOKENS_TO_MERGE[15] = new TokenType[] { TokenType.Order, TokenType.By, TokenType.OrderBy };
+            TOKENS_TO_MERGE[16] = new TokenType[] { TokenType.Sleep, TokenType.For, TokenType.Sleep };
+            TOKENS_TO_MERGE[17] = new TokenType[] { TokenType.Plus, TokenType.Equals, TokenType.PlusEquals };
+            TOKENS_TO_MERGE[18] = new TokenType[] { TokenType.Minus, TokenType.Equals, TokenType.MinusEquals };
+            TOKENS_TO_MERGE[19] = new TokenType[] { TokenType.Multiply, TokenType.Equals, TokenType.MultiplyEquals };
+            TOKENS_TO_MERGE[20] = new TokenType[] { TokenType.Divide, TokenType.Equals, TokenType.DivideEquals };
+            TOKENS_TO_MERGE[21] = new TokenType[] { TokenType.Percent, TokenType.Equals, TokenType.PercentEquals };
+            TOKENS_TO_MERGE[22] = new TokenType[] { TokenType.Is, TokenType.Before, TokenType.IsBefore };
+            TOKENS_TO_MERGE[23] = new TokenType[] { TokenType.Is, TokenType.After, TokenType.IsAfter };
         }
     }
 }
