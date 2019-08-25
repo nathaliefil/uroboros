b2e413b3ec9e5b4f62690f69568a9d3347564049
Wojciech-Filowiat <woj-fil@o2.pl>
2019-08-25 13:38:47 +0200

    Overall highlighting

--- MetaFileManager/gui/MainBoxGraphics.cs
+++ MetaFileManager/gui/MainBoxGraphics.cs
@@ -14,27 +14,55 @@ namespace Uroboros.gui
 
         private const int MIN_WINDOW_WIDTH = 600;
         private const int MIN_WINDOW_HEIGHT = 200;
-
-        Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Italic);
-
-
+        Style CommentStyle = new TextStyle(Brushes.Green, null, FontStyle.Italic);
+        Style CardinalStyle = new TextStyle(Brushes.Black, null, FontStyle.Bold);
+        Style UsualStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
+        Style InnerVariablesStyle = new TextStyle(Brushes.DarkViolet, null, FontStyle.Regular);
 
 
         private void CodeBoxSettings()
         {
             codeBox.AcceptsTab = true;
             codeBox.AutoIndent = true;
+            codeBox.AutoIndentChars = false;
         }
 
         private void codeBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             // paint comments
-            Range range = (sender as FastColoredTextBox).VisibleRange;
-            range.ClearStyle(GreenStyle);
-            range.SetStyle(GreenStyle, @"//.*$", RegexOptions.Multiline);
-            range.SetStyle(GreenStyle, @"(/\*.*?\*/)|(/\*.*)", RegexOptions.Singleline);
-            range.SetStyle(GreenStyle, @"(/\*.*?\*/)|(.*\*/)", RegexOptions.Singleline |
+            Range wideRange = (sender as FastColoredTextBox).VisibleRange;
+            wideRange.ClearStyle(CommentStyle);
+            wideRange.SetStyle(CommentStyle, @"//.*$", RegexOptions.Multiline);
+            wideRange.SetStyle(CommentStyle, @"(/\*.*?\*/)|(/\*.*)", RegexOptions.Singleline);
+            wideRange.SetStyle(CommentStyle, @"(/\*.*?\*/)|(.*\*/)", RegexOptions.Singleline |
                         RegexOptions.RightToLeft);
+
+
+            // paint cardinal keywords
+            e.ChangedRange.ClearStyle(CardinalStyle);
+            e.ChangedRange.SetStyle(CardinalStyle, @"\b(?i)(add|ask|clear bin|clear log|copy|create|cut|delete|
+                |directory|drop|file|log off|log on|move|open|order|paste|print|reaccess|recreate|remodify|
+                |remove|rename|reverse|run|select|sleep|swap)(?-i)\b");
+
+
+            // paint usual keywords
+            e.ChangedRange.ClearStyle(UsualStyle);
+            e.ChangedRange.SetStyle(UsualStyle, @"\b(?i)(after|and|asc|before|by|desc|each|else|
+                |first|for|force|from|if|in|inside|is|last|like|not|or|order by|skip|to|unique|where|while|with|
+                |without|xor)(?-i)\b");
+
+
+            // paint inner variables keywords, (true,false), time elements
+            e.ChangedRange.ClearStyle(InnerVariablesStyle);
+            e.ChangedRange.SetStyle(InnerVariablesStyle, @"\b(access|creation|directories|empty|exist|
+                |extension|everything|files|fullname|index|iscorrect|isdirectory|isfile|location|modification|
+                |name|now|size|this)\b");
+            e.ChangedRange.SetStyle(InnerVariablesStyle, @"\b(?i)(false|true)(?-i)\b");
+            e.ChangedRange.SetStyle(InnerVariablesStyle, @"\b(?i)(century|decade|year|month|weekday|day|hour|
+                |minute|second|centuries|decades|years|months|days|hours|minutes|seconds)(?-i)\b");
+
+
+            // todo: clear bin, clear log, log on, log off, uroboros stop, empty list, order by
         }
 
         private void MainForm_Resize(object sender, System.EventArgs e)
