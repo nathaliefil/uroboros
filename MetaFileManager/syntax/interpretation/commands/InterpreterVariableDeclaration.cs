c2a459d18ae258297fb41a46880115070b887c06
Wojciech-Filowiat <woj-fil@o2.pl>
2019-08-28 18:43:59 +0200

    Var interpreter corrections

--- MetaFileManager/syntax/interpretation/commands/InterpreterVariableDeclaration.cs
+++ MetaFileManager/syntax/interpretation/commands/InterpreterVariableDeclaration.cs
@@ -38,34 +38,25 @@ namespace Uroboros.syntax.interpretation.commands
                     InterVariables.GetInstance().Add(name, InterVarType.Bool);
                     return new BoolDeclaration(name, (IBoolable)value);
                 }
+                else if (value is INumerable)
+                {
+                    InterVariables.GetInstance().Add(name, InterVarType.Number);
+                    return new NumericDeclaration(name, (INumerable)value);
+                }
+                else if (value is ITimeable)
+                {
+                    InterVariables.GetInstance().Add(name, InterVarType.Time);
+                    return new TimeDeclaration(name, (ITimeable)value);
+                }
+                else if (value is IStringable)
+                {
+                    InterVariables.GetInstance().Add(name, InterVarType.String);
+                    return new StringDeclaration(name, (IStringable)value);
+                }
                 else
                 {
-                    if (value is INumerable)
-                    {
-                        InterVariables.GetInstance().Add(name, InterVarType.Number);
-                        return new NumericDeclaration(name, (INumerable)value);
-                    }
-                    else
-                    {
-                        if (value is ITimeable)
-                        {
-                            InterVariables.GetInstance().Add(name, InterVarType.Time);
-                            return new TimeDeclaration(name, (ITimeable)value);
-                        }
-                        else
-                        {
-                            if (value is IStringable)
-                            {
-                                InterVariables.GetInstance().Add(name, InterVarType.String);
-                                return new StringDeclaration(name, (IStringable)value);
-                            }
-                            else
-                            {
-                                InterVariables.GetInstance().Add(name, InterVarType.List);
-                                return new ListDeclaration(name, value);
-                            }
-                        }
-                    }
+                    InterVariables.GetInstance().Add(name, InterVarType.List);
+                    return new ListDeclaration(name, value);
                 }
             }
             else
@@ -88,32 +79,26 @@ namespace Uroboros.syntax.interpretation.commands
                             throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be numeric.");
                         return new NumericDeclaration(name, value);
                     }
+                    else if (ivar.IsTime())
+                    {
+                        ITimeable value = TimeableBuilder.Build(tokens);
+                        if (value.IsNull())
+                            throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be time.");
+                        return new TimeDeclaration(name, value);
+                    }
+                    else if (ivar.IsString())
+                    {
+                        IStringable value = StringableBuilder.Build(tokens);
+                        if (value.IsNull())
+                            throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be text.");
+                        return new StringDeclaration(name, value);
+                    }
                     else
                     {
-                        if (ivar.IsTime())
-                        {
-                            ITimeable value = TimeableBuilder.Build(tokens);
-                            if (value.IsNull())
-                                throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be time.");
-                            return new TimeDeclaration(name, value);
-                        }
-                        else
-                        {
-                            if (ivar.IsString())
-                            {
-                                IStringable value = StringableBuilder.Build(tokens);
-                                if (value.IsNull())
-                                    throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be text.");
-                                return new StringDeclaration(name, value);
-                            }
-                            else
-                            {
-                                IListable value = ListableBuilder.Build(tokens);
-                                if (value.IsNull())
-                                    throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be list.");
-                                return new ListDeclaration(name, value);
-                            }
-                        }
+                        IListable value = ListableBuilder.Build(tokens);
+                        if (value.IsNull())
+                            throw new SyntaxErrorException("ERROR! Value assigned to variable " + name + " must be list.");
+                        return new ListDeclaration(name, value);
                     }
                 }
             }
